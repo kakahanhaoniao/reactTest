8b0ead0a14215acef6d43c6a3d7c3d398ba22ce2
xiao shao <494018648@qq.com>
2017-05-05 00:06:11 +0800

    fix test react

--- src/index.js
+++ src/index.js
@@ -12,11 +12,5 @@ component();
 //     )
 //   }
 // }
-if (__DEV__) {
-  console.warn('Extra logging');
-}
-// ...
-if (__PRERELEASE__) {
-  showSecretFeature();
-}
+
 ReactDOM.render(<Button />, document.getElementById('app'));
--- src/index2.js
+++ src/index2.js
@@ -2,4 +2,6 @@
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-alert('sdfsdf');
+import Test from '../components/Test/test';
+
+ReactDOM.render(<Test />, document.getElementById('app2'));
